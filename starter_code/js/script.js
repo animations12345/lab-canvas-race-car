522ca79d434e21101e41b81a7b9ee6ed8ef1395e
saruit0105 <Administrator@UM-1WL5462.cgcent.miami.edu>
2019-10-30 12:19:15 -0400

    changes

--- starter_code/js/script.js
+++ starter_code/js/script.js
@@ -1,7 +1,12 @@
 window.onload = function() {
-    document.getElementById('start-button').onclick = function() {
-        startGame();
-    };
+  document.getElementById("start-button").onclick = function() {
+    startGame();
+  };
 
-    function startGame() {}
+  document.getElementById("start").onclick = startGame;
+  let theGame;
+  function startGame() {
+    theGame = new Game();
+    mainLoop();
+  }
 };

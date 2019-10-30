3b5bf2014241f581da6e80785ab7c3b498d89887
saruit0105 <Administrator@UM-1WL5462.cgcent.miami.edu>
2019-10-30 14:23:03 -0400

    changes

--- starter_code/js/script.js
+++ starter_code/js/script.js
@@ -1,29 +1,31 @@
-window.onload = function() {
-  const ctx = document.getElementById("gamePage").getContext("2d");
-  let frames = 0;
-  function mainLoop() {
-    frames++;
-    console.log("clearRect occurred.");
-    ctx.clearRect(0, 0, 200, 500);
+const ctx = document.getElementById("gamePage").getContext("2d");
+let frames = 0;
+function mainLoop() {
+  frames++;
+  console.log("clearRect occurred.");
+  ctx.clearRect(0, 0, 200, 500);
 
-    // this is where we draw the hero
-    // drawSelf(theGame.theHero, false);
-    // then we draw all the obstacles
-    // theGame.obstacleArray.forEach(eachObstacle => {
-    //     drawSelf(eachObstacle, true);
-    // });
+  // this is where we draw the hero
+  // drawSelf(theGame.theHero, false);
+  // then we draw all the obstacles
+  // theGame.obstacleArray.forEach(eachObstacle => {
+  //     drawSelf(eachObstacle, true);
+  // });
 
-    // if (frames % 100 === 0) {
-    //     theGame.spawnObstacle();
-    // }
+  // if (frames % 100 === 0) {
+  //     theGame.spawnObstacle();
+  // }
 
-    requestAnimationFrame(mainLoop);
-  }
+  requestAnimationFrame(mainLoop);
+}
 
-  document.getElementById("start-button").onclick = startGame;
-  let theGame;
-  function startGame() {
-    theGame = new Game();
-    mainLoop();
-  }
-};
+document.getElementById("start-button").onclick = startGame;
+let theGame;
+function startGame() {
+  theGame = new Game();
+  mainLoop();
+}
+
+function test() {
+  console.log("hello");
+}

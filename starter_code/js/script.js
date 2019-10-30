3a40f27bd34409aaac2f1f1e4c8282a1f5907811
Julian Phillips <julian.p@me.com>
2019-10-30 12:52:24 -0400

    Fixed Start Game button, added game.css.

--- starter_code/js/script.js
+++ starter_code/js/script.js
@@ -1,8 +1,29 @@
 window.onload = function() {
-  document.getElementById("start-button").onclick = startGame;
-  let theGame;
-  function startGame() {
-    theGame = new Game();
-    mainLoop();
-  }
+    const ctx = document.getElementById('gamePage').getContext('2d');
+    let frames = 0;
+    function mainLoop() {
+        frames++;
+        console.log('clearRect occurred.');
+        ctx.clearRect(0, 0, 200, 500);
+
+        // this is where we draw the hero
+        // drawSelf(theGame.theHero, false);
+        // then we draw all the obstacles
+        // theGame.obstacleArray.forEach(eachObstacle => {
+        //     drawSelf(eachObstacle, true);
+        // });
+
+        // if (frames % 100 === 0) {
+        //     theGame.spawnObstacle();
+        // }
+
+        requestAnimationFrame(mainLoop);
+    }
+
+    document.getElementById('start-button').onclick = startGame;
+    let theGame;
+    function startGame() {
+        theGame = new Game();
+        mainLoop();
+    }
 };

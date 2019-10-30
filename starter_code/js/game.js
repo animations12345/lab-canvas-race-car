88b55e0125c3250a4ad697cfd7730b1a6fad4de8
Julian Phillips <julian.p@me.com>
2019-10-30 15:54:24 -0400

    drawSelf fuction implimented, added missile png, expanded Obstacle and Game classes, tweaked script.js.

--- starter_code/js/game.js
+++ starter_code/js/game.js
@@ -3,4 +3,22 @@ class Game {
         this.theCar = new Car(100, 500);
         this.obstacleArray = [];
     }
+
+    spawnObstacle() {
+        let rX = Math.floor(Math.random() * 200);
+        let rY = Math.floor(Math.random() * 200);
+        let rWidth = Math.floor(Math.random() * 50) + 10;
+        let rHeight = Math.floor(Math.random() * 50) + 10;
+
+        let newObstacle = new Obstacle(
+            missileTemplate,
+            rX,
+            rY,
+            rWidth,
+            rHeight,
+        );
+        this.obstacleArray.push(newObstacle);
+        // newObstacle.drawMissile();
+        newObstacle.moveDownForever();
+    }
 }
--- starter_code/js/script.js
+++ starter_code/js/script.js
@@ -1,29 +1,36 @@
-window.onload = function() {
-    const ctx = document.getElementById('gamePage').getContext('2d');
-    let frames = 0;
-    function mainLoop() {
-        frames++;
-        console.log('clearRect occurred.');
-        ctx.clearRect(0, 0, 200, 500);
+const ctx = document.getElementById('gamePage').getContext('2d');
+let frames = 0;
 
-        // this is where we draw the hero
-        // drawSelf(theGame.theHero, false);
-        // then we draw all the obstacles
-        // theGame.obstacleArray.forEach(eachObstacle => {
-        //     drawSelf(eachObstacle, true);
-        // });
+const missileTemplate = new Image();
+missileTemplate.src = '../images/missile.png';
+function drawSelf(obs) {
+    if (obs) {
+        ctx.drawImage(obs.img, obs.x, obs.y);
+    }
+}
+function mainLoop() {
+    frames++;
+    console.log('clearRect occurred.');
+    ctx.clearRect(0, 0, 200, 500);
+    // ctx.drawImage(missile, 0, frames);
 
-        // if (frames % 100 === 0) {
-        //     theGame.spawnObstacle();
-        // }
+    // this is where we draw the hero
+    // drawSelf(theGame.theHero, false);
+    // then we draw all the obstacles
+    theGame.obstacleArray.forEach(eachObstacle => {
+        drawSelf(eachObstacle, true);
+    });
 
-        requestAnimationFrame(mainLoop);
+    if (frames % 10 === 0) {
+        theGame.spawnObstacle();
     }
 
-    document.getElementById('start-button').onclick = startGame;
-    let theGame;
-    function startGame() {
-        theGame = new Game();
-        mainLoop();
-    }
-};
+    requestAnimationFrame(mainLoop);
+}
+
+document.getElementById('start-button').onclick = startGame;
+let theGame;
+function startGame() {
+    theGame = new Game();
+    mainLoop();
+}

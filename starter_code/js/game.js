9e640ea39ecead34ffb681ee85983ec9b5eca3a5
saruit0105 <Administrator@UM-1WL5462.cgcent.miami.edu>
2019-10-30 17:38:27 -0400

    changes

--- starter_code/js/game.js
+++ starter_code/js/game.js
@@ -4,19 +4,11 @@ class Game {
     this.obstacleArray = [];
   }
 
-<<<<<<< HEAD
-    spawnObstacle() {
-        let rX = Math.floor(Math.random() * 350) + 65;
-        let rY = 0;
-        let rWidth = Math.floor(Math.random() * 50) + 10;
-        let rHeight = Math.floor(Math.random() * 50) + 10;
-=======
   spawnObstacle() {
     let rX = Math.floor(Math.random() * 400);
     let rY = Math.floor(Math.random() * 400);
     let rWidth = Math.floor(Math.random() * 50) + 10;
     let rHeight = Math.floor(Math.random() * 50) + 10;
->>>>>>> 6159f98ef3cdc41a62ca01df24e2ef66d26d8c7a
 
     let newObstacle = new Obstacle(missileTemplate, rX, rY, rWidth, rHeight);
     this.obstacleArray.push(newObstacle);
--- starter_code/js/script.js
+++ starter_code/js/script.js
@@ -1,37 +1,37 @@
-const ctx = document.getElementById('gamePage').getContext('2d');
+const ctx = document.getElementById("gamePage").getContext("2d");
 let frames = 0;
 
 const missileTemplate = new Image();
-missileTemplate.src = './images/missile.png';
+missileTemplate.src = "./images/missile.png";
 function drawSelf(obs) {
-    if (obs) {
-        ctx.drawImage(obs.img, obs.x, obs.y);
-    } else {
-        ctx.drawImage();
-    }
+  if (obs) {
+    ctx.drawImage(obs.img, obs.x, obs.y);
+  } else {
+    ctx.drawImage(img, this.x, this.y, this.width, this.height);
+  }
 }
 function mainLoop() {
-    frames++;
-    console.log('clearRect occurred.');
-    ctx.clearRect(0, 0, 500, 500);
+  frames++;
+  console.log("clearRect occurred.");
+  ctx.clearRect(0, 0, 500, 500);
 
-    // this is where we draw the hero
-    drawSelf(theGame.theCar, false);
-    // then we draw all the obstacles
-    theGame.obstacleArray.forEach(eachObstacle => {
-        drawSelf(eachObstacle, true);
-    });
+  // this is where we draw the hero
+  drawSelf(theGame.theCar, false);
+  // then we draw all the obstacles
+  theGame.obstacleArray.forEach(eachObstacle => {
+    drawSelf(eachObstacle, true);
+  });
 
-    if (frames % 100 === 0) {
-        theGame.spawnObstacle();
-    }
+  if (frames % 100 === 0) {
+    theGame.spawnObstacle();
+  }
 
-    requestAnimationFrame(mainLoop);
+  requestAnimationFrame(mainLoop);
 }
 
-document.getElementById('start-button').onclick = startGame;
+document.getElementById("start-button").onclick = startGame;
 let theGame;
 function startGame() {
-    theGame = new Game();
-    mainLoop();
+  theGame = new Game();
+  mainLoop();
 }

86a40d1c4186237b27efe2ace0fc871437251e56
saruit0105 <Administrator@UM-1WL5462.cgcent.miami.edu>
2019-10-30 16:05:53 -0400

    pull

--- starter_code/js/car.js
+++ starter_code/js/car.js
@@ -1,3 +1,10 @@
+const canvas = document.getElementById("gamePage").getContext("2d");
+const img = new Image();
+img.src = "./images/car.png";
+img.onload = () => {
+  ctx.drawImage(img, 250, 400, 75, 75);
+};
+
 class Car {
   constructor(x, y, width, height) {
     this.x = x;
@@ -6,3 +13,26 @@ class Car {
     this.height = height;
   }
 }
+Car.prototype.move = moveCar;
+
+function moveCar(futureX, futureY) {
+  if (futureX + this.width <= 500 && futureX >= 0 && futureY + this.height <= 500 && futureY >= 0) {
+    this.x = futureX;
+    this.y = futureY;
+  }
+}
+
+document.onkeydown = function(e) {
+  if (e.key === "ArrowUp") {
+    theGame.theCar.x++;
+  }
+  if (e.key === "ArrowDown") {
+    theGame.theCar.move(theGame.theCar.x, theGame.theCar.y);
+  }
+  if (e.key === "ArrowLeft") {
+    theGame.theCar.move(theGame.theCar.x, theGame.theCar.y);
+  }
+  if (e.key === "ArrowRight") {
+    theGame.theCar.move(theGame.theCar.x, theGame.theCar.y);
+  }
+};
--- starter_code/js/game.js
+++ starter_code/js/game.js
@@ -3,4 +3,22 @@ class Game {
     this.theCar = new Car(100, 500);
     this.obstacleArray = [];
   }
+  collisionDetect(futureX, futureY) {
+    let canMove = true;
+
+    this.obstacleArray.forEach(obs => {
+      console.log(futureX, futureY, this.theCar.width, this.theCar.height, obs.x, obs.y, obs.width, obs.height);
+
+      if (
+        futureX + this.theCar.width >= obs.x &&
+        futureX <= obs.x + obs.width &&
+        futureY + this.theCar.height >= obs.y &&
+        futureY <= obs.y + obs.height
+      ) {
+        canMove = false;
+      }
+    });
+
+    return canMove;
+  }
 }
